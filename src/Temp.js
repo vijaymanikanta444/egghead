24e8cd42fac592e98b13bbffbacd824279b2c10a
vijaymanikanta <vijayakp444@gmail.com>
2020-06-20 21:30:13 +0530

    ref specification

---
+++ src/Temp.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export default class Temp extends Component {
+    static propTypes = {
+        name: PropTypes.string
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
