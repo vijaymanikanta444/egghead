7de9a01da8e2e46b1538af5258fd1fecd621a977
vijaymanikanta <vijayakp444@gmail.com>
2020-06-21 02:26:15 +0530

    react.child utilities

--- src/App.js
+++ src/App.js
@@ -1,47 +1,25 @@
 import React from 'react'
-import './App.css'
 
 class App extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      input : '/* Add your JSX here */',
-      output : '',
-      err : ''
-    }
-  }
-  update(e){
-     let code = e.target.value; 
-     try {
-      this.setState({
-        output: window.Babel
-        .transform(code, {presets: ['es2015', 'react']})
-        .code,
-        err:''
-      })
-     }
-     catch(err){
-      this.setState({err: err.message})
-     }
-  }
-
-
-  render() {
+  render(){
     return (
-      <div>
-        <header>{this.state.err}</header>
-        <div className="container">
-          <textarea
-          onChange={this.update.bind(this)}
-          defaultValue={this.state.input}/>
-          <pre>
-            {this.state.output}
-          </pre>
-        </div>
-      </div>
+      <Parent>
+        <div className="childA"></div>
+      </Parent>
     )
   }
 }
 
+class Parent extends React.Component {
+  render(){
+    // console.log(this.props.children)
+    // let items = React.Children.map(this.props.children, child => child)
+    // let items = React.Children.forEach(this.props.children, child => console.log(child.props.className))
+    // let items = React.Children.toArray(this.props.children)
+    let items = React.Children.only(this.props.children)
+    console.log(items)
+    return null
+  }
+}
 
 export default App
